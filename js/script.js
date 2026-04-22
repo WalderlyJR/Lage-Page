01419fe32e86b12fdd5f46a27ba4ea6f5fce14c3
WalderlyJR <walderlyjunior123@gmail.com>
2026-04-22 14:22:34 -0300

    ajuste video

--- js/script.js
+++ js/script.js
@@ -47,10 +47,36 @@ document.addEventListener("DOMContentLoaded", () => {
      PROVA SOCIAL (notificação)
   ========================= */
   const names = [
-    "Lucas", "Ana", "Rafael", "Mariana", "João",
-    "Fernanda", "Carlos", "Juliana", "Bruno", "Camila"
-  ];
+  "Lucas", "Ana COMPROU", "Rafael", "Mariana", "João",
+  "Fernanda", "Carlos", "Juliana COMPROU", "Bruno", "Camila",
 
+  "Pedro", "Larissa COMPROU", "Gabriel", "Amanda", "Felipe",
+  "Bianca", "Rodrigo", "Tatiane", "Diego", "Vanessa",
+
+  "Matheus", "Letícia COMPROU", "Gustavo", "Patrícia", "Vinicius",
+  "Priscila", "André", "Renata", "Leonardo", "Simone",
+
+  "Thiago COMPROU", "Beatriz", "Eduardo", "Carla", "Marcelo",
+  "Natália", "Caio", "Aline", "Henrique", "Cristiane",
+
+  "Fernando", "Kelly", "Ricardo", "Débora", "Alexandre",
+  "Sandra", "Danilo", "Jéssica", "Wesley", "Paula",
+
+  "Roberto", "Michele", "Fábio", "Sabrina", "Maurício",
+  "Monique", "Jonathan", "Rafaela", "Otávio", "Larissa",
+
+  "Igor", "Bruna", "César COMPROU", "Juliano", "Adriana",
+  "Alan", "Eliane", "Douglas COMPROU", "Elaine", "Robson",
+
+  "Leandro", "Valéria", "Samuel", "Fabiana", "Victor",
+  "Viviane", "William", "Cláudia", "Renan", "Daniele",
+
+  "Marcos", "Rosana", "Elias", "Gisele", "Sérgio",
+  "Marlene", "Paulo", "Isabela", "Cristiano", "Lorena",
+
+  "Arthur", "Yasmin", "Enzo", "Gabriela", "Heitor",
+  "Luana", "Miguel", "Alice", "Davi", "Helena"
+];
   const notification = document.getElementById("liveNotification");
   const nameEl = document.getElementById("liveName");
 
@@ -68,7 +94,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   // aparece a cada 5 segundos
-  setInterval(showNotification, 5000);
+  setInterval(showNotification, 25000);
 
   // primeira aparição rápida
   setTimeout(showNotification, 2000);

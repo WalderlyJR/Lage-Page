bf1d09640cb72a71661ef033593c663dd214a23d
walderlyjunior <155538836+WalderlyJR@users.noreply.github.com>
2026-04-22 15:49:48 -0300

    Add social proof notification featurea

--- js/script.js
+++ js/script.js
@@ -43,6 +43,60 @@ document.addEventListener("DOMContentLoaded", () => {
   revealOnScroll();
 
 
-  
+  /* =========================
+     PROVA SOCIAL (notificação)
+  ========================= */
+  const names = [
+  "Lucas", "Ana COMPROU", "Rafael", "Mariana", "João",
+  "Fernanda", "Carlos", "Juliana COMPROU", "Bruno", "Camila",
+
+  "Pedro", "Larissa COMPROU", "Gabriel", "Amanda", "Felipe",
+  "Bianca", "Rodrigo", "Tatiane", "Diego", "Vanessa",
+
+  "Matheus", "Letícia COMPROU", "Gustavo", "Patrícia", "Vinicius",
+  "Priscila", "André", "Renata", "Leonardo", "Simone",
+
+  "Thiago COMPROU", "Beatriz", "Eduardo", "Carla", "Marcelo",
+  "Natália", "Caio", "Aline", "Henrique", "Cristiane",
+
+  "Fernando", "Kelly", "Ricardo", "Débora", "Alexandre",
+  "Sandra", "Danilo", "Jéssica", "Wesley", "Paula",
+
+  "Roberto", "Michele", "Fábio", "Sabrina", "Maurício",
+  "Monique", "Jonathan", "Rafaela", "Otávio", "Larissa",
+
+  "Igor", "Bruna", "César COMPROU", "Juliano", "Adriana",
+  "Alan", "Eliane", "Douglas COMPROU", "Elaine", "Robson",
+
+  "Leandro", "Valéria", "Samuel", "Fabiana", "Victor",
+  "Viviane", "William", "Cláudia", "Renan", "Daniele",
+
+  "Marcos", "Rosana", "Elias", "Gisele", "Sérgio",
+  "Marlene", "Paulo", "Isabela", "Cristiano", "Lorena",
+
+  "Arthur", "Yasmin", "Enzo", "Gabriela", "Heitor",
+  "Luana", "Miguel", "Alice", "Davi", "Helena"
+];
+  const notification = document.getElementById("liveNotification");
+  const nameEl = document.getElementById("liveName");
+
+  function showNotification() {
+    if (!notification || !nameEl) return;
+
+    const randomName = names[Math.floor(Math.random() * names.length)];
+    nameEl.textContent = randomName;
+
+    notification.classList.add("show");
+
+    setTimeout(() => {
+      notification.classList.remove("show");
+    }, 3000);
+  }
+
+  // aparece a cada 5 segundos
+  setInterval(showNotification, 35000);
+
+  // primeira aparição rápida
+  setTimeout(showNotification, 9000);
 
 });
